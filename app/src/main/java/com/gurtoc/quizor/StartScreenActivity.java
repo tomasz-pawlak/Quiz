dee4a920cb3bea5e91b34f2caf3668d2e340eaed
gurtoc <gurtoc@gmail.com>
2018-12-28 14:04:32 +0100

    Pierwszy listener

--- app/src/main/java/com/gurtoc/quizor/StartScreenActivity.java
+++ app/src/main/java/com/gurtoc/quizor/StartScreenActivity.java
@@ -1,7 +1,10 @@
 package com.gurtoc.quizor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class StartScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        Button buttonStartButton = findViewById(R.id.button_rozpocznij_quiz);
+        buttonStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startQuiz();
+            }
+        });
+
+    }
+    private void startQuiz(){
+        Intent intent = new Intent(StartScreenActivity.this, QuizActivity.class);
+        startActivity(intent);
     }
 }
+
